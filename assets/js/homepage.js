a01289029a7367fbf473f48d1d97859ea0352df2
LoiTran <tranloi781@gmail.com>
2022-06-10 03:15:53 +0700

    issues by HUGE topic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,16 @@
-
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -24,7 +36,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl= document.querySelector("#language-buttons")
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -96,5 +108,17 @@ repoEl.appendChild(statusEl);
   }}
 
   
-  };
+};
 
+var buttonClickHandler= function(event){
+    var language= event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
